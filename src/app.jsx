b3e5079ea678c94a086fd03a5f7260e85f614468
callme-devil <pransermi@gmail.com>
2023-01-26 23:31:20 +0330

    feat: Added PresentationControls

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,4 @@
-import { ContactShadows, Environment, Float, OrbitControls, Stage } from '@react-three/drei'
+import { ContactShadows, Environment, Float, OrbitControls, PresentationControls, Stage } from '@react-three/drei'
 import { Suspense } from 'react'
 import Mac from './components/Mac'
 
@@ -8,12 +8,18 @@ export default function App()
 
         <Environment preset='city'/>
         <color args={['#695b5b']} attach='background'/>
-        <OrbitControls makeDefault />
         
         <Suspense fallback={null}>
-            <Float rotationIntensity={0.4}>
-                <Mac position-y ={-1.2} />
-            </Float>
+            <PresentationControls 
+                global
+                rotation={[0.13 , 0.1 , 0]}
+                polar ={[-0.4 , 0.2]}
+                azimuth ={[-1 , 0.75]}
+            >
+                <Float rotationIntensity={0.4}>
+                    <Mac position-y ={-1.2} />
+                </Float>
+            </PresentationControls>
 
             {/* <ContactShadows opacity={0.4} scale={5} blur={2.4} position-y={-0.4}/> */}
         </Suspense>
