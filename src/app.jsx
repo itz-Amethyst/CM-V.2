d5c7c3f0f6259eb47644dedb26a2d5b5c9755575
callme-devil <pransermi@gmail.com>
2023-01-26 23:26:36 +0330

    feat: Configured mac model

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,21 @@
-import { OrbitControls } from '@react-three/drei'
+import { ContactShadows, Environment, Float, OrbitControls, Stage } from '@react-three/drei'
+import { Suspense } from 'react'
+import Mac from './components/Mac'
 
 export default function App()
 {
     return <>
 
+        <Environment preset='city'/>
+        <color args={['#695b5b']} attach='background'/>
         <OrbitControls makeDefault />
         
-        <mesh>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
+        <Suspense fallback={null}>
+            <Float rotationIntensity={0.4}>
+                <Mac position-y ={-1.2} />
+            </Float>
 
+            {/* <ContactShadows opacity={0.4} scale={5} blur={2.4} position-y={-0.4}/> */}
+        </Suspense>
     </>
 }
