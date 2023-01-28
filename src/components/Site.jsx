eeaeb5ce097d35c8c1084d980aed11473b259976
callme-devil <pransermi@gmail.com>
2023-01-28 23:59:53 +0330

    feat: added site component

---
+++ src/components/Site.jsx
@@ -0,0 +1,13 @@
+import { Html } from "@react-three/drei";
+
+export default function Site(props){
+      //? Html can hide behind geometry using the occlude prop. but only works on ortographic camera and more not in PresentationControls
+    return <Html wrapperClass="htmlScreen" transform  {...props} >
+
+            {/* <p>tst</p>
+            <button>fdsf</button> */}
+            <iframe src="https://callme-milad.pages.dev/" />
+      </Html>
+
+    
+}
