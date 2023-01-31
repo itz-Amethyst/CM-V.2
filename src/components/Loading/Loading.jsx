9f47a3ee3b646786d600881f00ce62f2dbfb9ad4
callme-devil <pransermi@gmail.com>
2023-01-31 21:00:31 +0330

    feat: added loading comp

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,11 +1,28 @@
-// import './style.css'
-import { Html } from "@react-three/drei";
+import './style.css'
 import gsap from "gsap";
+import { useState } from 'react';
+import Progress from './Progress';
 
 export default function Loading(){
-  
+
+  const[hasClicked , setHasClicked] = useState(false)
+
+  const element = document.querySelector('button')
+
+  // const loading = document.querySelector('.loading')
+
+
+  const toggleClicked = () =>{
+    setHasClicked(hasClicked)
+
+    //? bug need to add loading class to progresss comp
+    // loading.remove()
+
+    element.remove()
+
+  }
+
   return <>
-  <Html>
       <div className="container">
 
       <div className="blocks">
@@ -19,67 +36,18 @@ export default function Loading(){
 
       <div className="letters">
         <div className="row">
-          <div className="letter top-left">T</div>
-          <div className="letter top-right">2</div>
+          {/* <div className="letter top-left">T</div> */}
+          <div className="letter loading"><Progress/></div>
+
+          <div className="letter button"><button onClick={toggleClicked}>Click to start</button></div>
+          {/* <div className="letter top-right">2</div> */}
         </div>
         <div className="row">
-          <div className="letter bottom-right">7</div>
+          {/* <div className="letter bottom-right">7</div> */}
         </div>
       </div>
 
 
     </div>
-  </Html>
   </>
-}
-
-function AnimateGsap(){
-    gsap.from('.letter',0.8 ,{
-        y:-20,
-        opacity:0,
-        ease:'power3.inOut',
-        stagger:0.1
-      })
-      
-      gsap.to('.top-left , .top-right',2 ,{
-        top:"0",
-        ease:'power3.inOut',
-        delay:2,
-      })
-      
-      gsap.to('.bottom-right',2 ,{
-        bottom:"0",
-        ease:'power3.inOut',
-        delay:2,
-      })
-      
-      gsap.to('.top-left',2 ,{
-        left:"0",
-        ease:'power3.inOut',
-        delay:4,
-      })
-      
-      gsap.to('.top-right',2 ,{
-        right:"0",
-        ease:'power3.inOut',
-        delay:4,
-      })
-      
-      gsap.to('.bottom-right',2 ,{
-        right:"0",
-        ease:'power3.inOut',
-        delay:4,
-      })
-      
-      gsap.to('.block-left',2 ,{
-        left:"-50%",
-        ease:'power3.inOut',
-        delay:4,
-      })
-      
-      gsap.to('.block-right',2 ,{
-        right:"-50%",
-        ease:'power3.inOut',
-        delay:4,
-      })
 }
