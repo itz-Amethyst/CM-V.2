53d903dec2143ebce96024ca01e85c84e137fc20
callme-devil <pransermi@gmail.com>
2023-01-31 20:59:37 +0330

    feat: placed the loading comp in right place

--- src/index.jsx
+++ src/index.jsx
@@ -2,18 +2,23 @@ import './css/style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import App from './app.jsx'
+import Loading from './components/Loading/Loading'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
-    <Canvas
-        camera={ {
-            fov: 45,
-            near: 0.1,
-            far: 2000,
-            position: [ -3, 1.5, 4 ]
-        } }
-    >
-        <App />
-    </Canvas>
+    <>
+        <Loading/>
+
+        <Canvas
+            camera={ {
+                fov: 45,
+                near: 0.1,
+                far: 2000,
+                position: [ -3, 1.5, 4 ]
+            } }
+        >
+            <App/>
+        </Canvas>
+    </>
 )
