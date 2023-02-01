08e00e2d5a930d66629a160b03f058e73ddcf684
callme-devil <pransermi@gmail.com>
2023-02-01 22:27:51 +0330

    feat: configured progress

--- src/components/Loading/Progress.jsx
+++ src/components/Loading/Progress.jsx
@@ -1,89 +1,47 @@
 import { useProgress } from "@react-three/drei"
 import gsap from "gsap";
-import { useEffect } from "react";
+import { useMemo } from "react";
+import LoadingMessages from "../common/Loadingmessages";
 
 export default function Progress(){
     const { active, progress, errors, item, loaded, total } = useProgress()
+
+    var LoadingText = useMemo(() =>{
+
+      var LoadingText = Math.floor(Math.random()*LoadingMessages.length);
+
+      return LoadingText
+    },[])
+
+    var text = LoadingMessages[LoadingText]
+
     if(loaded === 9){
     //    Animate()
     DisLoading()
     }
     console.log(active, progress, errors, item, loaded, total);
-    return <div>
+    return <div className="loading">
+              <div className="randomText">
+                <span>{text}</span>
+              </div>
             <span>
-                {progress} % loaded
+                {progress} % loaded 
             </span>
         </div>
 }
 
-function Animate(){
-    gsap.from('.letter',0.8 ,{
-        y:-20,
-        opacity:0,
-        ease:'power3.inOut',
-        stagger:0.1
-      })
-      
-      gsap.to('.top-left , .top-right',2 ,{
-        top:"0",
-        ease:'power3.inOut',
-        delay:2,
-      })
-      
-      gsap.to('.bottom-right',2 ,{
-        bottom:"0",
-        ease:'power3.inOut',
-        delay:2,
-      })
-      
-      gsap.to('.top-left',2 ,{
-        left:"0",
-        ease:'power3.inOut',
-        delay:2,
-      })
-      
-      gsap.to('.top-right',2 ,{
-        right:"0",
-        ease:'power3.inOut',
-        delay:2,
-      })
-      
-      gsap.to('.bottom-right',2 ,{
-        right:"0",
-        ease:'power3.inOut',
-        delay:2,
-      })
-      
-      gsap.to('.block-left',2 ,{
-        left:"-50%",
-        ease:'power3.inOut',
-        delay:2,
-      })
-      
-      gsap.to('.block-right',2 ,{
-        right:"-50%",
-        ease:'power3.inOut',
-        delay:2,
-      })
-  
-      gsap.to('.container',2,{
-        height:'0vh',
-        ease:'power3.inOut',
-        delay:3,
-      })
-}
 
 function DisLoading(){
-    gsap.to('.loading' , 0.8 ,{
-        opacity:0,
-        ease:'power3.inOut',
-    })
+  gsap.to('.loading' , 0.8 ,{
+      opacity:0,
+      ease:'power3.inOut',
+  })
 
-    gsap.to('.button' , 0.8 ,{
-        opacity:1,
-        ease:'power3.inOut',
-    })
+  gsap.to('.button' , 1.2 ,{
+      opacity:1,
+      ease:'power3.inOut',
+  })
 
-    document.querySelector('.button').style.pointerEvents = "all"
+  document.querySelector('.button').style.pointerEvents = "all"
 
 }
