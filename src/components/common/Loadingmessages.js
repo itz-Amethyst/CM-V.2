0cc5810a5cc00d7815835878fb5c25bb2567175d
callme-devil <pransermi@gmail.com>
2023-02-01 22:28:02 +0330

    feat: added loadingmessages

---
+++ src/components/common/Loadingmessages.js
@@ -0,0 +1,10 @@
+const LoadingMessages = ["Reticulating splines...",
+    "Generating witty dialog...",
+    "Swapping time and space...",
+    "Spinning violently around the y-axis...",
+    "Tokenizing real life...",
+    "Bending the spoon...",
+    "Filtering morale...",
+    "Don't think of purple hippos...",]
+
+export default LoadingMessages
