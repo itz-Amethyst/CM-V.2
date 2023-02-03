d87e3bfb1f1bc47062a31ca0408d69cf75819f36
callme-devil <pransermi@gmail.com>
2023-02-03 20:05:22 +0330

    (Clean Up): loading

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,44 +1,11 @@
 import './style.css'
-import gsap from "gsap";
-import { useEffect, useMemo, useState } from 'react';
-import Progress from './Progress';
 import Entry from './Entry';
 
 export default function Loading(){
 
- 
-
-  const element = document.querySelector('.button')
-
-  // const loading = document.querySelector('.loading')
-
-
-  const toggleClicked = () =>{
-    setHasClicked(hasClicked)
-
-    useEffect(() =>{
-      element.style.opacity = null
-    element.style.pointerEvents = null
-    },[])
-
-    //? bug need to add loading class to progresss comp
-    // loading.remove()
-
-    // element.remove()
-
-
-  }
-
-  // const color = useMemo(() =>{
-  //   element.style.opacity = null
-  //   element.style.pointerEvents = null
-  // }, [hasClicked])
-
   return <>
     <div className="container">
       <Entry/>
-
-
     </div>
   </>
 }
